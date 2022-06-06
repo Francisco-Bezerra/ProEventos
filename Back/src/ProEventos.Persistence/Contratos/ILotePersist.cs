ab720b03c640ed6fddc9e08827c6b3c13a2ae002
Francisco Bezerra <chagasfranciscobezerra88@gmail.com>
2022-06-06 18:20:04 -0300

    Seção 11 - Eventos e Lotes

---
+++ Back/src/ProEventos.Persistence/Contratos/ILotePersist.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ProEventos.Domain;
+
+namespace ProEventos.Persistence.Contratos
+{
+    public interface ILotePersist
+    {
+        //EVENTOS
+        Task<Lote[]> GetLotesByEventoIdAsync(int eventoId);
+        Task<Lote> GetLoteByIdsAsync(int eventoId, int id);
+
+    }
+}
